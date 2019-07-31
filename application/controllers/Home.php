73fe8c0060ebd30ad571d5b750fe20e1a0d918b2
Arif Hidayat <arief21.hidayat21@gmail.com>
2019-07-31 20:40:56 +0700

    #3 Controllers & Views

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,11 @@
 <?php
 
 class Home extends CI_Controller{
-    public function index(){
-        echo "Home / index";
+    public function index($nama = ""){
+        $data["judul"] = "Halaman Home";
+        $data["nama"] = $nama;
+        $this->load->view("templates/header", $data);
+        $this->load->view("home/index");
+        $this->load->view("templates/footer");
     }
 }

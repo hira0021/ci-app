6729d0256c81df9d11ec2dc3ed7d52f6ce8d724e
Arif Hidayat <arief21.hidayat21@gmail.com>
2019-08-07 14:27:00 +0700

    #7.5 better flasher

--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -7,25 +7,34 @@
                 <div class="card-header">
                     Form Tambah Data Mahasiswa
                 </div>
-                <div class="card-body">
-                    <?php if(validation_errors()): ?>
-                            <div class="alert alert-danger" role="alert">
-                                <?= validation_errors();?>
-                            </div>
-                    <?php endif;?>
-                    
+                <div class="card-body">                    
                     <form action="" method="post">
                         <div class="form-group">
                             <label for="nama">Nama</label>
                             <input name="nama" type="text" class="form-control" id="nama">
+                            <small class="form-text text-danger">
+                                <?php if(form_error('nama')): ?>
+                                    <?= form_error('nama');?>
+                                <?php endif;?>
+                            </small>
                         </div>
                         <div class="form-group">
                             <label for="nim">Nim</label>
                             <input name="nim" type="text" class="form-control" id="nim">
+                            <small class="form-text text-danger">
+                                <?php if(form_error('nim')): ?>
+                                    <?= form_error('nim');?>
+                                <?php endif;?>
+                            </small>
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
                             <input name="email" type="text" class="form-control" id="email">
+                            <small class="form-text text-danger">
+                                <?php if(form_error('email')): ?>
+                                    <?= form_error('email');?>
+                                <?php endif;?>
+                            </small>
                         </div>
                         <div class="form-group">
                             <label for="jurusan">Jurusan</label>
